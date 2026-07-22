197e2bb7321d0e4cb372a451082d11e362d37dd8
Vaibhav Bakal <129209971+VaibhavBakal@users.noreply.github.com>
2026-07-22 23:35:11 +0530

    Update CorsConfig.java

--- src/main/java/com/example/demo/configuration/CorsConfig.java
+++ src/main/java/com/example/demo/configuration/CorsConfig.java
@@ -4,18 +4,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class CorsConfig {
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
 
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("*");
-			}
-		};
-	}
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://attendencemanagement-frontend.onrender.com"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+
+            }
+        };
+    }
 }
+
